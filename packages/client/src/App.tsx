64af824c5933255aa39d913089266ae2cd10bcb2
nadav-galili <nadavg1000@gmail.com>
2025-09-01 21:41:34 +0300

    set up prettier

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,17 +1,28 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
+import { Button } from './components/ui/button';
 
 function App() {
-  const [message, setMessage] = useState("");
+   const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    fetch("/api/hello")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message));
-  }, []);
+   useEffect(() => {
+      fetch('/api/hello')
+         .then((res) => res.json())
+         .then((data) => setMessage(data.message));
+   }, []);
 
-  return (
-    <p className="p-4 text-4xl text-center font-bold underline">{message}</p>
-  );
+   return (
+      <div className="p-4">
+         <p className="p-4 text-4xl text-center font-bold underline">
+            {message}
+         </p>
+         <Button>Click me</Button>
+         <Button variant="destructive">Click me</Button>
+         <Button variant="outline">Click me</Button>
+         <Button variant="secondary">Click me</Button>
+         <Button variant="ghost">Click me</Button>
+         <Button variant="link">Click me</Button>
+      </div>
+   );
 }
 
 export default App;
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,6 +1,6 @@
-import express from "express";
-import type { Request, Response } from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import type { Request, Response } from 'express';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
@@ -8,13 +8,13 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World");
+app.get('/', (req: Request, res: Response) => {
+   res.send('Hello World');
 });
-app.get("/api/hello", (req: Request, res: Response) => {
-  res.send({ message: "Hello World" });
+app.get('/api/hello', (req: Request, res: Response) => {
+   res.send({ message: 'Hello World' });
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+   console.log(`Server is running on http://localhost:${port}`);
 });
