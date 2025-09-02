5ce1f0cb92e933aafa22d82425057c8cda202789
nadav-galili <nadavg1000@gmail.com>
2025-09-02 09:43:53 +0300

    form

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,21 +1,8 @@
-import { useState, useEffect } from 'react';
-import { Button } from './components/ui/button';
-
+import ChatBot from './components/ChatBot';
 function App() {
-   const [message, setMessage] = useState('');
-
-   useEffect(() => {
-      fetch('/api/hello')
-         .then((res) => res.json())
-         .then((data) => setMessage(data.message));
-   }, []);
-
    return (
       <div className="p-4">
-         <p className="p-4 text-4xl text-center font-bold underline">
-            {message}
-         </p>
-         <Button>Click me</Button>
+         <ChatBot />
       </div>
    );
 }
